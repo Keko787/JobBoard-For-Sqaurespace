43f8d247a92c32495e799343666a454ef5da7b6a
Kevin S Kostage <73184531+Keko787@users.noreply.github.com>
2023-06-29 14:52:42 -0400

    Update data.js

--- jobBoardGithub/data/data.js
+++ jobBoardGithub/data/data.js
@@ -1,30 +1,72 @@
 let jobsdata = [  /* You can add more positions and information based on the format below */
     /* { "id": #, 
+      "title": "Position", 
+      "description": "example ",  // (you can add html elements like <strong> to make text bold and <ul> and <li> to makes lists, the only exception is <p> because it is the default in the modal window)
+      "location": "City, State ",  // (abbreiviate State)
+      "locationSlug": "city-state",  // (make sure its lowercase and a dash between city and state, otherwise you may have to use the slugify api and lowercase)
+      "department": "Department"
+    },  // the comma matters if you want to add another entry after, the last entry doesnt need a comma at the end
+
+    // you can copy this as a template
+    { "id": #, 
       "title": "position", 
-      "description": "example (you can add html elements except for <p> because it is the default)",
-      "location": "City, State (abbreiviate State)", 
-      "department": "Department Name" 
-    }, */
+      "description": "example ",  
+      "location": "City, State ",  
+      "locationSlug": "city-state",  
+      "department": "Department"
+    },
+    */
     
     { "id": 1, 
       "title": "Front-end Developer", 
-      "description": "test124",
-      "location": "New York, USA", 
-      "department": "Development" 
+      "description": "<strong> please work </strong> again",
+      "location": "Jacksonville, FL", 
+      "locationSlug": "jacksonville-fl",
+      "department": "Development"
+      
     },
     {
       "id": 2,
       "title": "Backend Developer",
-      "description": "We are hiring a backend developer to work on our server infrastructure.",
-      "location": "London, UK",
+      "description": "We are hiring a backend developer to work on our server infrastructure. <ul>
+          <li> test 1. </li>
+          <li> test 2. </li>
+          <li> test 3. </li>
+          </ul>",
+      "location": "Pittsburgh, PA",
+      "locationSlug": "pittsburgh-pa",
       "department": "Development"
     },
     {
       "id": 3,
-      "title": "UX Designer",
+      "title": "UX Designer NOT LOL",
       "description": "Join our UX design team and help create intuitive user experiences.",
-      "location": "San Francisco, USA",
+      "location": "Pittsburgh, PA",
+      "locationSlug": "pittsburgh-pa",
       "department": "Design"
+    },
+    {
+      "id": 4,
+      "title": "UX Designess",
+      "description": "Join our UX design team and help creas.",
+      "location": "Jacksonville, FL",
+      "locationSlug": "jacksonville-fl",
+      "department": "Design"
+    },
+    {
+      "id": 5,
+      "title": "Engineer",
+      "description": "Join us to plan and build things.",
+      "location": "Jacksonville, FL",
+      "locationSlug": "jacksonville-fl",
+      "department": "Engineering"
+    },
+    {
+      "id": 6,
+      "title": "Drafter",
+      "description": "Join us.",
+      "location": "Jacksonville, FL",
+      "locationSlug": "jacksonville-fl",
+      "department": "Drafting"
     }
   ]
-  
